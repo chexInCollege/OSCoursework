3d332322e583385b3889765e7c7fd9f1813286c4
chexInCollege <grovesap@g.cofc.edu>
2023-11-10 16:07:05 -0500

    update

--- PROJECT2/project2.c
+++ PROJECT2/project2.c
@@ -3,11 +3,42 @@
 #include <string.h>
 #include <pthread.h>
 
+typedef struct {
+    int id;
+} packet;
+
+void *threadRoutine(void *arg) {
+    packet *info = (packet*) arg;
+
+    printf("Thread %d :3\n", info->id);
+
+    return NULL;
+}
+
 int main(int argc, char **argv) {
     if ( argc < 2 ) {
         fprintf(stderr, "usage: project2 <numConsumers>\n");
         exit(1); 
     }
 
+    // initialize main thread vars
+    int i;
+    int numConsumers = atoi(argv[1]);
+
+
+    pthread_t *threadList = malloc(numConsumers * sizeof(pthread_t));
+    // packet    *threadInfo[numConsumers];
+ 
+    for(i = 0; i < numConsumers; i++) {
+        packet *info = malloc(sizeof(packet));
+        info->id = i;
+        pthread_create(&threadList[i], NULL, threadRoutine, info);
+    } 
+
+
+    // wait for all threads to terminate
+    for(i = 0; i < numConsumers; i++) {
+        pthread_join(threadList[i], NULL);
+    }
 
 }
